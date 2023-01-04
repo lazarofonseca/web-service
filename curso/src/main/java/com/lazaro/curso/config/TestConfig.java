8be0e8d04dc5085610344fe4f944401792a71f7d
Lázaro <lazaronobrega3@gmail.com>
2023-01-03 21:52:27 -0300

    Many-to-many association with JoinTable

--- curso/src/main/java/com/lazaro/curso/config/TestConfig.java
+++ curso/src/main/java/com/lazaro/curso/config/TestConfig.java
@@ -49,8 +49,18 @@ public class TestConfig implements CommandLineRunner{
 		Product p5 = new Product(null,"The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		
 		
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
 		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -63,8 +73,6 @@ public class TestConfig implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderrepository.saveAll(Arrays.asList(o1, o2, o3));
 		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		
 	}
